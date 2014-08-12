90f0238cafd59c8493d5285f58f584490bb78d56
Angelo Sembrano <anobilisgorse@gmail.com>
2014-08-12 16:52:55 +0800

    Change name

--- db_script/progresshistory.sql
+++ db_script/progresshistory.sql
@@ -1,4 +1,4 @@
-CREATE TABLE progress_history(
+CREATE TABLE progresshistory(
 	user_id int primary key,
 	height int,
 	weight int,
@@ -17,15 +17,15 @@ $$
   declare
      v_uid int;
   begin
-     select into v_uid user_id from progress_history
+     select into v_uid user_id from progresshistory
         where user_id = p_uid;
 
      if v_uid isnull then
-        insert into progress_history values
+        insert into progresshistory values
         (p_uid, p_h, p_w, p_bmi, p_s, p_age, p_progperc);  
 
      else
-        update progress_history
+        update progresshistory
            set height = p_h, weight = p_w, bmi = p_bmi, status = p_s, age = p_age, prog_perc = p_progperc
            where user_id = p_uid;
      end if;
@@ -35,7 +35,7 @@ $$
 $$
   language 'plpgsql';
   
---insert into progress_history values (1, 180, 67, 18.0, 'normal', 18, 0);
+--insert into progresshistory values (1, 180, 67, 18.0, 'normal', 18, 0);
 --select setproghist(2, 167, 80, 20, 'overweight', 18, 0);  
   
 create or replace function
@@ -44,11 +44,11 @@ create or replace function
 
 $$
 
-    select user_id, height, weight, bmi, status, age, prog_perc from progress_history
+    select user_id, height, weight, bmi, status, age, prog_perc from progresshistory
     where user_id = $1;
     
 $$
 language 'sql';
 
---select * from progress_history where user_id = 1;
+--select * from progresshistory where user_id = 1;
 --getproghist(2);
