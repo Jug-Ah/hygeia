ec8b737af380bc1a2102b685d802e339b23fa121
anobilisgorse <anobilisgorse@gmail.com>
2014-08-14 23:57:17 +0800

    Progress History Update (JSON)

--- db_script/fitnessplan.sql
+++ db_script/fitnessplan.sql
@@ -1,4 +1,4 @@
-create table fitness_plan(
+create table fitnessplan(
   planId int primary key,
   ageBracket text,
   gender text,
@@ -8,21 +8,21 @@ create table fitness_plan(
 );
 
 create or replace
-    function setfitness_plan(p_planId int, p_ageBracket text, p_gender text, p_healthBracket text, p_exercisePlan text, p_dietPlan text)
+    function setfitnessplan(p_planId int, p_ageBracket text, p_gender text, p_healthBracket text, p_exercisePlan text, p_dietPlan text)
     return text as
 $$
   declare
     v_planId int;
   begin
-    select into v_planId planId from fitness_plan
+    select into v_planId planId from fitnessplan
     where planId = p_planId;
 
     if v_planId isnull then
-      insert into fitness_plan(planId, ageBracket, gender, healthBracket, exercisePlan, dietPlan) values
+      insert into fitnessplan(planId, ageBracket, gender, healthBracket, exercisePlan, dietPlan) values
       (p_planId, p_ageBracket, p_gender, p_healthBracket, p_exercisePlan, p_dietPlan);
     else
 
-      update fitness_plan
+      update fitnessplan
         set ageBracket = p_ageBracket, gender = p_gender, healthBracket = p_healthBracket, exercisePlan = p_exercisePlan, dietPlan = p_dietPlan
         where planId = p_planId;
     end if;
@@ -35,7 +35,7 @@ $$
 
 
 create or replace function
-    get_fitness_plan(in int, out text, out text)
+    get_fitnessplan(in int, out text, out text)
     returns setof record as
 $$
       select exercisePlan, dietPlan
---
+++ javascript/globs_g.js
@@ -0,0 +1,32 @@
+//define functions and global variables here...
+var siteloc = "http://localhost/iGit";
+var scriptloc = "/scripts/"
+
+function fetchprogresshistory(id)
+{
+  $.ajax({
+      url: siteloc + scriptloc + "getproghist.py",
+      data: {id:id},
+      dataType: 'json',
+      success: function (res) {
+                  console.log(res);
+                  if(res[0][0] != "None")
+                  {
+					  table = '<table border="1">';
+					  for (i = 0; i < res.length; i++)
+					  {
+						  row = res[i];
+						  table += "<tr>";
+						  for (j = 0; j < row.length; j++)
+						  {
+							  table += "<td>" + row[j] + "</td>";
+						  }
+						  table += "</tr>";
+					  }
+					  table += "</table>";
+					  $("#target").html(table); 
+				  } // end if
+              }
+    });
+}
+
