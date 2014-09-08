0af7eaaa60ed8fe07643d6b82b35851c05e2e0b3
anobilisgorse <anobilisgorse@gmail.com>
2014-09-08 21:10:36 +0800

    Cleaned

--- README.txt
+++
@@ -1,4 +0,0 @@
-hygeia
-======
-
-Classified information
--- db_script/progressrecord.sql
+++ db_script/progressrecord.sql
@@ -1,5 +1,5 @@
-CREATE TABLE progressrecord(
-	userId int primary key,
+create table progressrecord(
+	userID serial primary key,  --(userId be 'int' soon and will be procured from useraccount.sql table)
 	height int,
 	weight int,
 	bmi double precision,
@@ -10,45 +10,47 @@ CREATE TABLE progressrecord(
 
 
 create or replace
-	function set_progressrecord(p_userId int, p_height int, p_weight int, p_bmi double precision, p_healthStatus text, p_age int, p_changePercentage double precision)
+	function set_progressrecord(p_height int, p_weight int, p_bmi double precision, p_healthStatus text, p_age int, p_changePercentage double precision) --(add first parameter 'p_userID int')
 	returns text as
 
 $$
-  declare
-     v_uid int;
+  --declare
+     --v_uid int;
   begin
-     select into v_uid user_id from progressrecord
-        where userId = p_uid;
+     --select into v_uid user_id from progressrecord
+        --where userID = p_uid;
 
-     if v_uid isnull then
+     --if v_uid isnull then
         insert into progressrecord values
-        (p_userId, p_height, p_weight, p_bmi, p_healthStatus, p_age, p_changePercentage);  
-
-     else
-        update progressrecord
-           set height = p_height, weight = p_weight, bmi = p_bmi, healthStatus = p_healthStatus, age = p_age, changePercentage = p_changePercentage
-           where userId = p_userId;
-     end if;
+        (p_height, p_weight, p_bmi, p_healthStatus, p_age, p_changePercentage);  
+		--(add first parameter 'p_userID')
+     --else
+        --update progressrecord
+           --set height = p_height, weight = p_weight, bmi = p_bmi, healthStatus = p_healthStatus, age = p_age, changePercentage = p_changePercentage
+           --where userID = p_userID;
+     --end if;
 
      return 'OK';
   end;	
 $$
   language 'plpgsql';
   
---insert into progresshistory values (1, 180, 67, 18.0, 'normal', 18, 0);
---select setproghist(2, 167, 80, 20, 'overweight', 18, 0);  
+--insert into progressrecord values (1, 180, 67, 18.0, 'normal', 18, 0);
+--select setprogressrecord(2, 167, 80, 20, 'overweight', 18, 0);  
   
 create or replace function
-	get_progressrecord(in int, out int, out int, out int, out double precision, out text, out int, out double precision)
+	get_progressrecord(in int, out int, out int, out double precision, out text, out int, out double precision)
 	returns setof record as
 
 $$
 
-    select userId, height, weight, bmi, healthStatus, age, changePercentage from progressrecord
-    where userId = $1;
+    select height, weight, bmi, healthStatus, age, changePercentage from progressrecord
+    where userID = $1;
     
 $$
 language 'sql';
 
---select * from progresshistory where user_id = 1;
---getproghist(2);
+
+
+--select * from progressrecord where user_id = 1;
+--getprogressrecord(2);
