30717f371d57ec3331b2db3493d6f11f5c350646
arleneyosoya <ayen.yosoya@gmail.com>
2014-09-12 02:51:08 +0800

    'additional'

--- db_script/personalInfo.sql
+++ db_script/personalInfo.sql
@@ -1,51 +1,36 @@
 ﻿create table personalinfo(
-   userId int primary key,
+   userId int references useraccount(userid),
+   infoID serial primary key,
    fullName text,
    birthday text,
-   age int,
    gender text,
    height int,
    weight int,
-   bmi double precision,
+   bmi numeric(4,2),
    healthStatus text
 );
 
 
 create or replace
-    function set_personalinfo(p_userId int, p_fullName text, p_birthday text, p_age int, p_gender text, p_height int, p_weight int, p_bmi double precision, p_healthStatus text) 
+    function set_personalinfo( userId int,  fullName text,  birthday text,  gender text,  height int,  weight int,  bmi numeric(4,2),  healthStatus text) 
     returns text as 
 $$
-  declare
-    v_id int;
-  begin
-    select into  v_id ID from personalinfo
-      where ID = p_id;
-
-    if v_id isnull then
-      insert into personalinfo(userId, fullName, birthday, age, gender, height, weight, bmi, healthStatus) values
-        (p_userId, p_fullName, p_birthday, p_age, p_gender, p_height, p_weight, p_bmi, p_healthStatus);
-    else
-
-      update personalinfo
-        set fullName = p_fullName, birthday = p_birthday, age = p_age, gender = p_gender, height = p_height, weight = p_weight, bmi = p_bmi, healthStatus = p_healthStatus
-        where userId = p_userId;
-    end if;
-    
-      return 'OK';
-  end;
-$$
-  language 'plpgsql';
+     begin
+      insert into personalinfo(userId, fullName, birthday,gender, height, weight, bmi, healthStatus) values
+        ( userId,  fullName,  birthday, gender,  height,  weight,  bmi,  healthStatus);
+    end;
+  language  'plpgsql';
 --how to use
 
 
 create or replace function
-     get_personalinfo(in int, out text, out text, out int, out text, out int, out int, out double precision, out text)
+     get_personalinfo(in int, out text, out text, out text, out int, out int, out numeric(4,2), out text)
      returns setof record as
 $$
-        select fullName, birthday, age, gender, height, weight, bmi, healthStatus from personalinfo
-        where userID = $1;
+     select fullName, birthday, gender, height, weight, bmi, healthStatus from personalinfo
+     where userId = $1;
         
 $$
-  language 'sql';
+  language  'sql';
 --how to use:
 
---
+++ scripts/personal_info.py
@@ -0,0 +1,24 @@
+from dosql_g import *
+import cgi
+import simplejson as json+ 
+
+
+def index(req, userID, fullname, birthday, gender, height, weight):
+    userID = cgi.escape(userID)
+	fullname = cgi.escape(fullname)
+	birthday = cgi.escape(birthday)
+	gender = cgi.escape(gender)
+	height = int(cgi.escape(height))
+	weight = int(cgi.escape(weight))
+		
+    x = doSql()
+    rets = x.execqry("select * from set_personalinfo(" + id + " , " + p_fullname + ", " + p_birthday + ", " + p_age + ", " + p_gender + ", " + p_height + ", " + p_weight + ", " + p_bmi + ", " + p_healthStatus + "); ", True)
+    result = []
+    for ret in rets:
+        stringed = map(str, ret)
+        result.append(stringed)
+
+    return json.dumps(result)
+ 
+ 
+ 
