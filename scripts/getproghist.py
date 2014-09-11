86845dd230c2c3d31dbb67cecbcfc74f1e6cdcf1
Angelo Sembrano <anobilisgorse@gmail.com>
2014-09-11 13:55:12 +0800

    Modified

--- scripts/getproghist.py
+++ scripts/getproghist.py
@@ -1,6 +1,7 @@
 <<<<<<< HEAD
 from dosql import *
 import cgi
+import math
 try:
     import json
 except ImportError:
@@ -12,13 +13,35 @@ import simplejson as json
 
 >>>>>>> 15afc79a86b6634998fefb6b4e4d858c813b9775
 
-def index(req, id):
-    id = cgi.escape(id)
+def index(req, height, weight, age):
+    height = cgi.escape(height)
+	weight = cgi.escape(weight)
+	age = cgi.escape(age) 
+	
+	b = bmi(height, weight)
+	s = status(b)
+	perc = 
+	
     x = doSql()
-    rets = x.execqry("select * from getproghist('" + id + "');", False)
+	#id is soon procured from useraccount.sql table
+    rec = x.execqry("select setprogressrecord('" + height + "', '" + weight + "', '" + b + "', '" + s + "', '" + age + "', '" + perc + "' );", True)
     result = []
-    for ret in rets:
-        stringed = map(str, ret)
+    for a in rec:
+        stringed = map(str, a)
         result.append(stringed)
 
     return json.dumps(result)
+
+#import this two functions from personalinfo.py once acquired	
+def bmi(height, weight):
+   return weight/ (height * height)
+   
+def status(bmi):	
+   if bmi < 18.50:  
+     return "underweight"
+   elif bmi >= 18.50 && bmi <= 24.99
+     return "normal"
+   elif bmi > 24.99:
+     return "overweight"
+
+def change_perc(c_bmi, p_bmi)
