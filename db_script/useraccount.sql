63568b6326f86a09156460eed20facb2df2aee54
Silver <silverserate64@gmail.com>
2014-09-11 19:45:38 -0800

    Improved Registation Files.
    - Added email field
    - Added input format validation
    - Added confirm password field.

--- db_script/useraccount.sql
+++ db_script/useraccount.sql
@@ -1,17 +1,19 @@
 create table useraccount (     
-     userId serial primary key,
-     username text,
-     userpass text
+	userid serial NOT NULL,
+	username text,
+	password text,
+	email text,
+	CONSTRAINT useraccount_pkey PRIMARY KEY (userid )
 );
 
 create or replace 
-	function set_useraccount(p_username text, p_userpass text)
+	function set_useraccount(p_username text, p_userpass text, p_useremail text)
 	returns text as
 	
 $$
    begin
-	     insert into useraccount(username, userpass) values
-		    (p_username, p_userpass);
+	     insert into useraccount(username, password, email) values
+		    (p_username, p_userpass, p_useremail);
       return 'success!';
    end;
    
